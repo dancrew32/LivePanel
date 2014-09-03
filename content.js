9d6b2d488aa778c9ef2a24a8e548ca5b90ca50d6
Dan Masquelier <dancrew32@gmail.com>
2014-09-03 14:43:52 -0700

    prevent bluring current filter selections with click()

--- content.js
+++ content.js
@@ -4,11 +4,16 @@
 	s.textContent = '('+ run.toString() +')()';
 	(document.getElementsByTagName('head')[0] || document.body || document.documentElement).appendChild(s);
 }(function() {
+	var blacklist = {
+		closest: '.filter_container',
+		input: 'textarea, input'
+	};
 	var watch = function() {
 		var context = $('#live');
 		var click = function() {
 			var current_focus = $(':focus');
-			if (!current_focus.is('textarea, input'))
+			var should_click = !current_focus.is(blacklist.input) && !current_focus.closest(blacklist.closest).length;
+			if (should_click)
 				$('.refresh_row', context).click()
 		};
 		setInterval(click, 2000);
